f6f7f727544b55f02b5190c7d9cc87457facfde5
APysnack <apysnack@gmail.com>
2022-05-21 18:00:01 -0400

    adds tailwind and apollo to connect client to GQL

---
+++ client/src/components/Users.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+// import UserAvatar from './UserAvatar';
+// import CreateUser from '.CreateUser';
+
+
+const GET_USERS = gql`
+{
+    users{
+        id
+        username
+    }
+}
+`
+
+function Users() {
+    const { loading, error, data } = useQuery(GET_USERS);
+
+    return (
+        <>  
+            {loading ? "Loading..." : 
+                data.users.map(user => (
+                        <div key={user.id}>{user.username}</div>
+                ))
+            }
+        </>
+    )
+}
+
+export default Users;
