d2e5d7f14efee7c445124385873d943dfac4a2c3
APysnack <apysnack@gmail.com>
2022-05-25 08:08:41 -0400

    minimalist UI forms user login and registration

---
+++ client/src/components/Homepage/Homepage.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { loginWithToken, logoutUser } from "../../redux/userState";
+import { useDispatch, useSelector } from "react-redux";
+import LoginForm from "../LoginForm/LoginForm";
+
+function Homepage() {
+  const [tokenPayload, setTokenPayload] = useState({});
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user.userState);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const callLoginWithToken = () => {
+    dispatch(loginWithToken(tokenPayload));
+  };
+
+  const callLogoutUser = () => {
+    dispatch(logoutUser(tokenPayload));
+  };
+
+  useEffect(() => {
+    let authToken = localStorage.getItem("auth_token");
+    if (authToken) {
+      let tempTokenPayload = {
+        headers: {
+          authorization: authToken,
+        },
+      };
+      setTokenPayload(tempTokenPayload);
+    } else {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (tokenPayload && Object.keys(tokenPayload).length > 0) {
+      callLoginWithToken();
+    }
+  }, [tokenPayload]);
+
+  useEffect(() => {
+    if (user?.email) {
+      setIsLoading(false);
+    }
+  }, [user]);
+
+  return (
+    <>
+      {isLoading ? (
+        "Loading..."
+      ) : user && !isLoading ? (
+        <div>
+          <h1>Logged in as {user.email}</h1>
+          <button style={{ color: "red" }} onClick={callLogoutUser}>
+            Log out
+          </button>
+        </div>
+      ) : (
+        <LoginForm />
+      )}
+    </>
+  );
+}
+
+export default Homepage;
